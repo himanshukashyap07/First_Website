38041db13691b2955df5cf7a0abaf885f4aca0a9
Himanshu Kashyap <168160199+himanshukashyap07@users.noreply.github.com>
2024-08-29 17:52:51 +0530

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -13,44 +13,44 @@ let clutter ='';
   siteName.innerHTML=clutter;
 })()
 
-const tl = gsap.timeline();
- gsap.from('.contact_link2', {
-  opacity:0,
-  x:40,
-  delay:1.5,
-  duration:1,
- })
- gsap.from('.contact_link3', {
-  opacity:0,
-  x:40,
-  delay:2.5,
-  duration:1,
-})
+// const tl = gsap.timeline();
+//  gsap.from('.contact_link2', {
+//   opacity:0,
+//   x:40,
+//   delay:1.5,
+//   duration:1,
+//  })
+//  gsap.from('.contact_link3', {
+//   opacity:0,
+//   x:40,
+//   delay:2.5,
+//   duration:1,
+// })
 
 
-tl.from('#LOGO',{
-  delay:1,
-  opacity:0,
-  y:50,
-  duration:1.5,
-})
+// tl.from('#LOGO',{
+//   delay:1,
+//   opacity:0,
+//   y:50,
+//   duration:1.5,
+// })
 
-tl.from('.nav-item',{
-  opacity:0,
-  y:35,
-  stagger:0.25,
-  duration:0.8,
-})
+// tl.from('.nav-item',{
+//   opacity:0,
+//   y:35,
+//   stagger:0.25,
+//   duration:0.8,
+// })
 
-tl.from('.mean-site-name-txt',{
-  opacity:0,
-  y:70,
-  duration:1,
-  stagger:{
-    each:0.15,
-    from:"center",
-  },
-})
+// tl.from('.mean-site-name-txt',{
+//   opacity:0,
+//   y:70,
+//   duration:1,
+//   stagger:{
+//     each:0.15,
+//     from:"center",
+//   },
+// })
 
 //video animation
 function LaptopVideoAnimation(){
@@ -201,36 +201,3 @@ if(screenWidth>1024){
 
 
 
-
-
-
-
-
-// gsap.from("#page1 .box",{
-//     scale:0,
-//     delay:1,
-//     duration:2,
-//     rotate:360,
-// })
-// //When user reach 2nd page using scrolling then this animation run otherwise it does not do anything 
-// gsap.from("#page2 .box",{
-//     scale:0,
-//     duration:2,
-//     rotate:360,
-//     scrollTrigger:{
-//         trigger:"#page2 .box",
-//         scroller:"body",
-//         markers:true,
-//         start:"top 60%",// when scroll hit this positon then animation start
-//         end:"top 30%",
-//         // scrub:true
-//         scrub:2, // value btw 1 to 5
-//         pin:true,
-//     }
-// })
-// gsap.from("#page3 .box",{
-//     scale:0,
-//     delay:1,
-//     duration:2,
-//     rotate:360,
-// })
